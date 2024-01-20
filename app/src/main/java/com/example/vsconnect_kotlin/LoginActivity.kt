0cdf096fdaef0fd96b2c98845f5d6469916a1978
Carlos Augusto Da Silva Santos <code.carlosaugusto@gmail.com>
2024-01-19 21:54:29 -0300

    inicialização do layout da tela servicos

--- app/src/main/java/com/example/vsconnect_kotlin/LoginActivity.kt
+++ app/src/main/java/com/example/vsconnect_kotlin/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.vsconnect_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.vsconnect_kotlin.databinding.ActivityLoginBinding
@@ -10,6 +11,24 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+
+        /*setOnClickListener é um ouvinte de clique
+        * ou seja, quando cliecar no botão Entrar, irá cair nesse bloco*/
+        binding.btnEntrar.setOnClickListener {
+
+            /*Variável mainIntent com a intenção de sair da LoginActivity e ir para MainActivity*/
+            val mainIntent = Intent(this@LoginActivity,
+                MainActivity::class.java)
+
+            /*executa a intenção armazenada na variável MainIntent*/
+            startActivity(mainIntent)
+
+            /*finaliza a Login Activity*/
+            finish()
+
+        }
+        setContentView(binding.root)
     }
 }
